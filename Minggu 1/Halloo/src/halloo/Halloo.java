515819bb2e33d6204109a9f7d1443a65e5f402c1
chairunisyacici <chairunisyacici2@gmail.com>
2025-02-12 10:27:11 +0700

    hallo
    hallo

--- Minggu 1/Halloo/src/halloo/Halloo.java
+++ Minggu 1/Halloo/src/halloo/Halloo.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication26;
+package halloo;
 
 /**
  *
  * @author Dell
  */
-public class JavaApplication26 {
+public class Halloo {
 
     /**
      * @param args the command line arguments
